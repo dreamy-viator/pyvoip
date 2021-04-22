008f47316f9a678cddad5b35e25f47a4b4e37dfa
jonghewk <jonghewk@linecorp.com>
2021-04-22 11:42:57 +0900

    simple signaling

---
+++ signaling_server.py
@@ -0,0 +1,50 @@
+import asyncio
+import websockets
+import json
+
+
+connected = set()
+rooms = dict()
+
+async def handler(websocket, path):
+    global connected
+    global rooms
+    # register
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            m = json.loads(message)
+            if m["cmd"] == "joinRoom":
+                roomId = m['roomId']
+                print('roomId :', roomId)
+                if roomId not in rooms.keys():
+                    rooms[roomId] = 1
+                elif rooms[roomId] >= 2:
+                    websocket.send(json.dumps({
+                            "cmd": "err",
+                            "reason" : "roomFull"
+                    }))
+                else:
+                    rooms[roomId] += 1
+                    await websocket.send(json.dumps({
+                        "cmd": "playStream"
+                    }))
+
+            elif m["cmd"] == "description":
+                for c in connected:
+                    if c != websocket:
+                        await c.send(message)
+
+            elif m["cmd"] == "candidate":
+                for c in connected:
+                    if c != websocket:
+                        await c.send(message)
+
+
+    finally:
+        # Unregister.
+        connected.remove(websocket)
+
+start_server = websockets.serve(handler, 'localhost', 8765)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
