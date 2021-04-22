e63b60cc903ffc3ae0a42e93a4af6573dbd46c24
Doyeon-k <54570091+Doyeon-k@users.noreply.github.com>
2021-04-22 13:57:26 +0900

    Update __init__.py

--- opuslib/opuslib/api/__init__.py
+++ opuslib/opuslib/api/__init__.py
@@ -13,16 +13,7 @@ __author__ = 'Никита Кузнецов <self@svartalf.info>'
 __copyright__ = 'Copyright (c) 2012, SvartalF'
 __license__ = 'BSD 3-Clause License'
 
-
-#lib_location = find_library('opus')
-#
-#if lib_location is None:
-#    raise Exception(
-#        'Could not find Opus library. Make sure it is installed.')
-
-#libopus = ctypes.CDLL(lib_location)
-
-libopus = ctypes.windll.LoadLibrary("../../Scripts/opus.dll")
+libopus = ctypes.windll.LoadLibrary("../../Scripts/opus.dll") # should adjust the path where opus.dll is exist
 
 c_int_pointer = ctypes.POINTER(ctypes.c_int)
 c_int16_pointer = ctypes.POINTER(ctypes.c_int16)
